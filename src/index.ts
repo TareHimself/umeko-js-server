7de292fd11e66c954e5d97d3f964e422fd35381e
Tare Ebelo <75279482+TareHimself@users.noreply.github.com>
2023-02-16 07:08:51 -0500

    cluster added

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,30 @@ try {
     throw new Error("Missing Secretes.json");
 }
 
-import './server'
+import './sqlite';
+import cluster from 'cluster';
+import * as os from 'os';
+import path from 'path';
+
+if (cluster.isPrimary) {
+
+    // Take advantage of multiple CPUs
+    const cpus = os.cpus().length;
+
+    if (process.argv.includes('--no-cluster')) {
+        cluster.fork(process.env);
+    }
+    else {
+        for (let i = 0; i < Math.max(cpus, 4); i++) {
+            cluster.fork(process.env);
+        }
+    }
+
+    cluster.on("exit", (worker, code) => {
+        if (code !== 0 && !worker.exitedAfterDisconnect) {
+            const nw = cluster.fork();
+        }
+    });
+} else {
+    require(path.join(__dirname, "server"));
+}
